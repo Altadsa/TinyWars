5c9ae46203ba56605a60b6d9b59dbffc3ac59201
Adam Coyle <acoyle22@qub.ac.uk>
2019-12-16 20:56:09 +0000

    Import Art Assets.

---
+++ src/RTS_New/Assets/UnitMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovement : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToMouse();
+        }
+
+        _animator.SetBool("moving", _agent.hasPath);
+    }
+
+    private void MoveToMouse()
+    {
+        var mousepos = Input.mousePosition;
+        var ray = Camera.main.ScreenPointToRay(mousepos);
+        RaycastHit hit;
+        bool hashit = Physics.Raycast(ray, out hit);
+        if (hashit)
+        {
+            _agent.SetDestination(hit.point);
+        }
+    }
+}
