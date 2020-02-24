2fad9c78dbc2367a257d6f025861a5a9f4a288e3
Adam Coyle <acoyle22@qub.ac.uk>
2020-02-24 19:10:58 +0000

    Add Build Menu UI.

---
+++ src/RTS_New/Assets/_scripts/Buildings/BuildMenuData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/BuildMenuData")]
+public class BuildMenuData : ScriptableObject
+{
+    [SerializeField] private Sprite _icon;
+    [SerializeField] private Building _buildingPrefab;
+
+    public Sprite Icon => _icon;
+    public Building Building => _buildingPrefab;
+}
---
+++ src/RTS_New/Assets/_scripts/UiBuildMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiBuildMenu : MonoBehaviour
+{
+    //When enabled, the build menu gets the available buildings for the player to purchase. Locked buildings should show their requirements and 
+    //should be greyed out to indicate they are unavailable.
+    
+    //Menu needs a reference to the player to inspect its data
+    [SerializeField] private PlayerController _controller;
+    [SerializeField] private Button[] _meunButtons;
+
+    [Header("Debug")] public BuildMenuData[] _menuData;
+    
+    private void OnEnable()
+    {
+        //var buildings = _controller.Player;
+        for (int i = 0; i < _meunButtons.Length; i++)
+        {
+            if (i < _menuData.Length)
+            {
+                _meunButtons[i].onClick.RemoveAllListeners();
+                _meunButtons[i].GetComponent<Image>().sprite = _menuData[i].Icon;
+                var building = _menuData[i].Building;
+                _meunButtons[i].onClick.AddListener(delegate
+                {
+                    var newBuilding = Instantiate(building); 
+                    newBuilding.Initialize(_controller.Player);
+                });
+                _meunButtons[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                _meunButtons[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
