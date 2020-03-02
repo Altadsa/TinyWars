67b54b16ef7a455863e6f7d3f9e2676affb281f9
Adam Coyle <acoyle22@qub.ac.uk>
2020-03-02 14:21:51 +0000

    Add rendered minimap.

---
+++ src/RTS_New/Assets/_scripts/UI/UiMinimap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UiMinimap : MonoBehaviour, IPointerDownHandler
+{
+    private Camera _mCamera;
+    public Transform PlayerParent;
+    public Camera MiniCam;
+    
+    private void Awake()
+    {
+        _mCamera = Camera.main;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Ray mRay = _mCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(mRay, out hit))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+        }
+    }
+}
