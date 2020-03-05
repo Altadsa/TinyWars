0a472f1ac7112d572b96c46b4a857b14da463b6b
Adam Coyle <acoyle22@qub.ac.uk>
2020-03-05 23:13:08 +0000

    Add dynamic sizing of tooltip window. Enables text blocks depending on type of menu item.

--- src/RTS_New/Assets/_scripts/UI/UiTooltip.cs
+++ src/RTS_New/Assets/_scripts/UI/UiTooltip.cs
@@ -13,10 +13,13 @@ public class UiTooltip : MonoBehaviour
     
     private const string REQ_TEXT = "Requirements:\n";
 
+    private RectTransform _tooltipRect;
+    
     private void Awake()
     {
         FindObjectsOfType<UiBuildingMenuButton>().ToList().ForEach(b => b.TooltipUpdated += UpdateTooltip);
         _tooltipGo.SetActive(false);
+        _tooltipRect = _tooltipGo.GetComponent<RectTransform>();
     }
 
     private void UpdateTooltip(MenuData data, bool setActive)
@@ -30,6 +33,26 @@ public class UiTooltip : MonoBehaviour
         _title.text = data.Name;
         _description.text = data.Description;
         _tooltipGo.SetActive(true);
+        if (data is Queueable queuable)
+        {
+            _requirements.gameObject.SetActive(true);
+            _requirements.text = REQ_TEXT;
+            var reqs = queuable.Requirements;
+            foreach (var req in reqs)
+            {
+                _requirements.text += $"\t-{req}";
+            }
+        }
+        else
+        {
+            _requirements.gameObject.SetActive(false);
+        }
+
+        var newSize = _tooltipRect.sizeDelta;
+        newSize.y = 10 + _title.GetComponent<RectTransform>().sizeDelta.y + _description.GetComponent<RectTransform>().sizeDelta.y;
+        if (_requirements.gameObject.activeInHierarchy)
+            newSize.y += _requirements.GetComponent<RectTransform>().sizeDelta.y;
+        _tooltipRect.sizeDelta = newSize;
     }
     
 }
