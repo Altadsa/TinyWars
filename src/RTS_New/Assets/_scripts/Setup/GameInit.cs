c1fd06df7aa899764962a81b6365ea4aaafbdde8
Adam Coyle <acoyle22@qub.ac.uk>
2020-04-15 09:32:23 +0100

    start units now consume food on creation.

--- src/RTS_New/Assets/_scripts/Setup/GameInit.cs
+++ src/RTS_New/Assets/_scripts/Setup/GameInit.cs
@@ -4,7 +4,7 @@ public class GameInit : MonoBehaviour
 {
     public int StartUnitCount = 1;
     public int PLayerCount = 1;
-    public Unit StartUnit;
+    public MenuDataUnit StartUnit;
     public Building StartBuilding;
     public PlayerController PlayerPrefab;
     public PlayerController AiPrefab;
@@ -39,19 +39,25 @@ public class GameInit : MonoBehaviour
             
         }
 
+        var unitFoodUse = StartUnit.Data.Food;
+        
         for (int i = 0; i < _players.Length; i++)
         {
+            
             var player = _players[i];
             var start = StartPositions[i].position;
-            for (int j = 0; j < StartUnitCount; j++)
-            {
-                var newUnit = Instantiate(StartUnit, start - 5 * Vector3.forward, Quaternion.identity);
-                newUnit.Initialize(player);                
-            }
-
+            
             var newBuilding = Instantiate(StartBuilding, start, StartBuilding.transform.rotation);
             newBuilding.Initialize(player);
             newBuilding.GetComponentInChildren<MeshRenderer>().material = player.EntityMaterial;
+            
+            for (int j = 0; j < StartUnitCount; j++)
+            {
+
+                player.ChangeFoodUsage(unitFoodUse);
+                StartUnit.Complete(newBuilding);                
+            }
+
         }
     }
     
