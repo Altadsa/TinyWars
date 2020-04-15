e5cb4c52c193ff15691f2d40b8b4b04b4ee78639
Adam Coyle <acoyle22@qub.ac.uk>
2020-04-15 16:50:19 +0100

    unit move speed now controlled by modifiers. add smoke fx to blacksmith.

--- src/RTS_New/Assets/_scripts/units/MoveAction.cs
+++ src/RTS_New/Assets/_scripts/units/MoveAction.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.AI;
 
 /// <summary>
 /// Basic Unit movement Script
@@ -10,6 +9,19 @@ public class MoveAction : UnitAction
 {
 
     public int Priority { get; } = 3;
+    
+    protected override void Start()
+    {
+        base.Start();
+        _agent.speed = _unit.GetModifierValue(Modifier.MoveSpeed);
+        _unit.ModifiersUpdated += UpdateModifiers;
+    }
+
+    private void UpdateModifiers()
+    {
+        _agent.speed = _unit.GetModifierValue(Modifier.MoveSpeed);
+    }
+    
     public override bool IsActionValid(GameObject targetGo, Vector3 targetPos)
     {
         StartCoroutine(Move(targetPos));
