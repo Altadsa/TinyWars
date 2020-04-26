d31ea65b77ec80f931ecfa756d0c7df14384653f
Adam Coyle <acoyle22@qub.ac.uk>
2020-04-26 11:37:16 +0100

    begin implementing unit selection ui.

--- src/RTS_New/Assets/_scripts/UI/UiUnitSelection.cs
+++ src/RTS_New/Assets/_scripts/UI/UiUnitSelection.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,10 +12,21 @@ public class UiUnitSelection : MonoBehaviour
     [SerializeField] private Transform _selection;
     private GameObject[] _selectionObjects;
 
+    [Header("****INFO PANEL****")] 
+    public Image Icon;
+
+    public Image Health;
+
+    public TMP_Text HealthText;
+
+    public TMP_Text DamageText;
+    
+    
     void Awake()
     {
         SetObjects();
         _sc.SelectionUpdated += SelectionUpdated;
+        _selectionUI.SetActive(false);
     }
 
     private void SelectionUpdated(List<Entity> units)
@@ -25,13 +37,19 @@ public class UiUnitSelection : MonoBehaviour
             return;
         }
         _selectionUI.SetActive(true);
+        SetInfo(units[0] as Unit);
         for (int i = 0; i < units.Count; i++)
         {
             var unit = units[i] as Unit;
-            
-            _selectionObjects[i].GetComponentInChildren<Image>().sprite = unit.Data.Icon;
+
+            _selectionObjects[i].GetComponent<Image>().sprite = unit.Data.Icon;
             _selectionObjects[i].SetActive(true);
-           // _selectionObjects[i].GetComponent<Button>().onClick.AddListener( delegate {  });
+            var button = _selectionObjects[i].GetComponent<Button>();
+            button.onClick.RemoveAllListeners();
+            button.onClick.AddListener(delegate
+            {
+                SetInfo(unit);
+            });
         }
 
         for (int i = units.Count; i < _selectionObjects.Length; i++)
@@ -40,12 +58,18 @@ public class UiUnitSelection : MonoBehaviour
         }
     }
 
+    private void SetInfo(Unit unit)
+    {
+        Icon.sprite = unit.Data.Icon;
+        
+    }
+    
     private void SetObjects()
     {
         _selectionObjects = new GameObject[_selection.childCount];
         for (int i = 0; i < _selection.childCount; i++)
         {
-            _selectionObjects[i] = _selection.GetChild(i).gameObject;
+            _selectionObjects[i] = _selection.GetChild(i).GetChild(0).gameObject;
             _selectionObjects[i].SetActive(false);
         }
     }
